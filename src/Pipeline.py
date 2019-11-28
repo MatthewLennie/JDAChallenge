e8b7f64cff485469ffe4be947536cf9086aa71bb
Matthew Lennie <matthewdlennie@gmail.com>
2019-11-28 15:24:07 +0100

    main functionality working. need to do assesment and build up the test cases:

--- src/Pipeline.py
+++ src/Pipeline.py
@@ -14,9 +14,35 @@ from sklearn.preprocessing import OneHotEncoder
 from sklearn.metrics import mean_absolute_error
 import config
 import sklearn.pipeline
-
-class Pipeline():
+import pickle
+class Preprocess():
+    """ Simple wrapper for the encoder for the weekdays. 
+    If you want to implement normalization etc.. here would be the spot. 
+    """
+    def __init__(self,config):
+        self.config = config
+        
+    def fit(self,df):
+        self.encoder = OneHotEncoder(sparse=False,categories='auto').fit(df['weekday'].values.reshape(-1,1))
+        
+        self.config.x_keys.extend([0,1,2,3,4,5]) #don't over determine the system
+        
+    def transform(self,df):
+        day_encoded = pd.DataFrame(self.encoder.transform(df['weekday'].values.reshape(-1,1)))   
+        df = pd.concat([df, day_encoded],axis=1)
+        if 'cnt' in df.keys():
+            self.y_data = df['cnt'].astype('float')
+            
+        #remove weekday from the keys. 
+        self.config.x_keys = [x for x in self.config.x_keys if x!='weekday']
+        
+        return df[self.config.x_keys].astype('float')
+    def fit_transform(self,df):
+        self.fit(df)
+        return self.transform(df)
     
+    
+class Pipeline():    
     def __init__(self,config):
         assert self.__check_config_file
         self.config = config
@@ -27,37 +53,64 @@ class Pipeline():
     def __check_config_file(self):
         return False
     
-    def __preprocess_features(self,df): 
-        enc = pd.DataFrame(OneHotEncoder(sparse=False,categories='auto').fit_transform(df['weekday'].values.reshape(-1,1)))
-        df = pd.concat([df, enc],axis=1)
-        self.config.x_keys.extend(enc.keys()[:-1]) #don't over determine the system
-        self.config.x_keys = [x for x in self.config.x_keys if x!='weekday']
-        return df
     
     def create_test_training_sets(self,df):
         """Encodes the weekday and performs test train split. 
         """
         #turn weekdays into one hot encode remove original feature
+        self.preprocessor = Preprocess(self.config)
         
+        x = self.preprocessor.fit_transform(df)
+        y = self.preprocessor.y_data
+        
+        self.config = self.preprocessor.config
+
         #%%
         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
-            df[self.config.x_keys].astype("float"), df[self.config.y_keys].astype("float"), test_size=0.33, random_state=self.config.random_seed)
+            x, y, test_size=0.33, random_state=self.config.random_seed)
 
-    def load_in_pretrained():
-        pass
     
     def hyper_parameter_search(self):
-        #To speed up set to DASK backend...
-        clf = xgb.XGBRegressor(n_jobs=self.config.n_jobs)
-        search = GridSearchCV(clf,self.config.parameters,cv=self.config.number_of_cross_validations)
-        search.fit(self.X_train,self.y_train)
-        self.model = search.best_estimator_
+        """performs a search of hyper-parameters on data. 
+        """
+        try:
+            clf = xgb.XGBRegressor(n_jobs=self.config.n_jobs)
+            search = GridSearchCV(clf,self.config.parameters,cv=self.config.number_of_cross_validations)
+            search.fit(self.X_train,self.y_train)
+            self.model = search.best_estimator_
+        except AttributeError:    
+            print("Have you created training data?")
+            
     
     def build_pipeline(self):
-        self.pipeline = sklearn.pipeline.Pipeline([('anova', anova_filter), ('svc', clf)])
-    
-    def predict(self,x_data):
+        """ Packs the process into a sklearn pipeline ready for inference. 
+        """
+        try:
+            self.pipeline = sklearn.pipeline.Pipeline([('preprocessor', self.preprocessor), ('XGmodel', self.model)])
+        except AttributeError:
+            print("the model hasn't been trained or loaded")
         
+    def pickle_pipeline(self,filename = "bicycle_tree.pkl"):
+        """pickles pipeline 
+        """
+        pickle.dump(self.model,filename)
+        
+    def unpickle_pipeline(self,filename = "bicycle_tree.pkl"):
+        """pickles pipeline 
+        """
+        self.model =pickle.load(filename)
+        
+    def predict(self,df):
+        """Easy access to the predict function. 
+        """
+        self.pipeline.predict(df)
+    
+    def retrain_model(self,df):
+        """Takes the model with the optimized hyperparameters and refits with
+        new data.
+        """
+        #TODO
+        raise NotImplementedError
     
     def assess_model():
         pass
@@ -68,11 +121,12 @@ if __name__ == "__main__":
     df = read_in_data(r'https://archive.ics.uci.edu/ml/machine-learning-databases/00275/Bike-Sharing-Dataset.zip')
     example_pipe = Pipeline(config)
     example_pipe.create_test_training_sets(df)
+    example_pipe.hyper_parameter_search()
+    example_pipe.build_pipeline()
     
     
-    
-    
-    
+    example_pipe.pipeline.predict(df)
+#    example_pipe.pipeline.(df)
     
     
     
--- src/config.py
+++ src/config.py
@@ -9,6 +9,6 @@ y_keys = 'cnt'
 random_seed = 42
 
 #HyperParameter Search 
-parameters = {'reg_lambda':[0.1,0.3,1],'reg_alpha':[0.1,0.3,1],'n_estimators':[100],'gamma':[0.1,0.2,0.9],'max_depth':[6,7,8,9,10,11,12,13,14],'learning_rate':[0.01,0.1,0.3,0.5]}
+parameters = {'reg_lambda':[0.1],'reg_alpha':[0.1],'n_estimators':[100],'gamma':[0.9],'max_depth':[10,11],'learning_rate':[0.5]}
 n_jobs = -1
 number_of_cross_validations =3
