4035461eb05f309110314db381ffa1da48d92e2f
Matthew Lennie <matthewdlennie@gmail.com>
2019-11-29 17:27:18 +0100

    main functionality working initial tests

--- src/Pipeline.py
+++ src/Pipeline.py
@@ -2,11 +2,17 @@
 """
 Created on Wed Nov 27 16:01:41 2019
 
+Preprocess class selects the keys and turns weekdays into a a one hot encoded
+Pipeline wraps up the code nessescary to train the XGBoost model with a hyper
+parameter search. 
+The config contains the main levers to adjust the training process. 
+
 @author: matt_
 """
 
 import seaborn as sns
 import matplotlib.pyplot as plt
+import numpy as np
 from sklearn.model_selection import train_test_split
 import xgboost as xgb
 from sklearn.model_selection import GridSearchCV
@@ -15,6 +21,8 @@ from sklearn.metrics import mean_absolute_error
 import config
 import sklearn.pipeline
 import pickle
+
+
 class Preprocess():
     """ Simple wrapper for the encoder for the weekdays. 
     If you want to implement normalization etc.. here would be the spot. 
@@ -42,14 +50,13 @@ class Preprocess():
         return self.transform(df)
     
     
-class Pipeline():    
+class Pipeline():
+    """ Enables the building of the pipeline for the bicycle problem. 
+    """    
     def __init__(self,config):
         assert self.__check_config_file
         self.config = config
         
-    def create_pipeline():
-        pass
-    
     def __check_config_file(self):
         return False
     
@@ -74,13 +81,15 @@ class Pipeline():
         """performs a search of hyper-parameters on data. 
         """
         try:
-            clf = xgb.XGBRegressor(n_jobs=self.config.n_jobs)
+            clf = xgb.XGBRegressor(n_jobs=self.config.n_jobs,objective='reg:squarederror')
             search = GridSearchCV(clf,self.config.parameters,cv=self.config.number_of_cross_validations)
-            search.fit(self.X_train,self.y_train)
+            search.fit(self.X_train,self.y_train.values)
+            self.search = search
             self.model = search.best_estimator_
         except AttributeError:    
             print("Have you created training data?")
             
+                
     
     def build_pipeline(self):
         """ Packs the process into a sklearn pipeline ready for inference. 
@@ -93,11 +102,13 @@ class Pipeline():
     def pickle_pipeline(self,filename = "bicycle_tree.pkl"):
         """pickles pipeline 
         """
+        raise NotImplementedError
         pickle.dump(self.model,filename)
         
     def unpickle_pipeline(self,filename = "bicycle_tree.pkl"):
         """pickles pipeline 
         """
+        raise NotImplementedError
         self.model =pickle.load(filename)
         
     def predict(self,df):
@@ -112,21 +123,49 @@ class Pipeline():
         #TODO
         raise NotImplementedError
     
-    def assess_model():
-        pass
-
+    def assess_model(self):
+        """Provides some basic diagnostics for the model. 
+        Compares the absolute errors to the absolute deviations to give an idea 
+        of the models predictive power. 
+        """
+        with plt.style.context('seaborn-darkgrid'):
+            #grab predictions
+            y_preds = self.model.predict(self.X_test)
+            mae = mean_absolute_error(y_preds,self.y_test)
+            print("Mean Absolute Error: {}".format(mae))
+            #Do Plotting
+            plt.figure()
+            fig = plt.hist(abs(y_preds-self.y_test),50,range=(0,1000),density=True, Label = "Absolute Errors from Model")
+            plt.vlines(mae,0,1,linewidth=4, color='r',Label="Mean Absolute Error from Model")
+            plt.gca().set_ylim([0,max(fig[0])*1.1])
+            plt.xlabel('Absolute Errors vs Absolute Deviations')
+            
+            #Create deviations
+            RandomA = self.y_test.sample(1000)
+            RandomB = self.y_test.sample(1000)
+            deviations = abs(RandomA.values-RandomB.values)
+            mad= np.mean((deviations))
+            print("Mean Absolute Deviations: {}".format(mad))
+            
+            #plot deviations
+            plt.hist(deviations,50,density=True,range=(0,1000),Label='Absolute Deviations from data',alpha=0.4)
+            plt.vlines(mad,0,1,linewidth=4, color='k',Label ='Mean Absolute Deviations')
+            plt.gca().set_ylim([0,max(fig[0])*1.1])
+            plt.legend()
+        
 
 if __name__ == "__main__":
-    
+#%% ## Example Usage of the code. 
     df = read_in_data(r'https://archive.ics.uci.edu/ml/machine-learning-databases/00275/Bike-Sharing-Dataset.zip')
     example_pipe = Pipeline(config)
     example_pipe.create_test_training_sets(df)
     example_pipe.hyper_parameter_search()
     example_pipe.build_pipeline()
+    example_pipe.assess_model()
     
-    
-    example_pipe.pipeline.predict(df)
-#    example_pipe.pipeline.(df)
+ #Not implemented functionatliy.    
+#    example_pipe.pickle_pipeline()
+#    example_pipe.retrain_model(df)
     
     
     
--- src/config.py
+++ src/config.py
@@ -9,6 +9,7 @@ y_keys = 'cnt'
 random_seed = 42
 
 #HyperParameter Search 
-parameters = {'reg_lambda':[0.1],'reg_alpha':[0.1],'n_estimators':[100],'gamma':[0.9],'max_depth':[10,11],'learning_rate':[0.5]}
+parameters = {'reg_lambda':[0.1,0.3,0.5,1],'reg_alpha':[0.1,0.3,1],'n_estimators':[100,200],'gamma':[0.1,0.2,0.9,0.01],'max_depth':[2,3,4,6,7,8,9,10,11,12,13,14],'learning_rate':[0.01,0.1,0.3,0.5,0.9]} 
+
 n_jobs = -1
 number_of_cross_validations =3
